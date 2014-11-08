8a80e9e26374d86428d174c67dd33c52e252fd0a
Jonas Tornfors <jt222ii@student.lnu.se>
2014-11-08 15:15:02 +0000

    started 1.4 tried a while to not use loops without success. Will focus on finishing the task then look back at not using loops

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,37 @@
 "use strict";
 
 var makePerson = function(persArr){
+    // Din kod här...
+    //ska innehålla information om den högsta åldern, lägsta åldern, medelåldern samt en sträng med samtliga namn sorterade i bokstavsordning
+    var minAge;
+    var maxAge;
+    var averageAge;
+    var names = "";
+    var nameArray = [];
+    var ageArray = [];
+    var result = {}; // taget från uppgiften 
 
+    for(var i=0;i<persArr.length;i++)
+    {
+       nameArray[i] = persArr[i]['name'];   //persArr[i].name; fungerar också
+    }
+    
+    
+        for(var i=0;i<persArr.length;i++)
+    {
+       ageArray[i] = persArr[i]['age'];   //persArr[i].name; fungerar också
+    }
+    
+    
+    nameArray.sort();
+    
+    names = nameArray.toString();
 
-	// Din kod här...
+	ageArray.sort();
+
 
 }
+var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}, {name: "Apan Hej", age: 46}];
+var result = makePerson(data);
 
+console.log(result);
