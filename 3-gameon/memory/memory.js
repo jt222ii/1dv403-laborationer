7de4cffffeb7108b5803b095bdd92e2b231d4485
Jonas Tornfors <jt222ii@student.lnu.se>
2014-11-26 19:31:45 +0000

    cards now successfully turn down after one second if you guessed wrong

--- 3-gameon/memory/memory.js
+++ 3-gameon/memory/memory.js
@@ -6,10 +6,15 @@ var memory = {
     columns: 4,
     count: 0,
     
-    test:0,
-    firsttry: null,
-    secondtry:null,
+    testerino:0,
     
+    points: 0,
+    
+    flippedImages:0,
+    test1:null,
+    test2:null,
+    
+    alink:null,
     img: null,
     cell: null,
     init:function(){
@@ -31,18 +36,23 @@ var memory = {
             for (var j = 0; j < memory.columns; j++)
             {
                 memory.cell = document.createElement("td");
+                memory.alink = document.createElement("a");
+                memory.alink.className = "pics/"+memory.randArray[memory.count]+".png";
                 memory.img = document.createElement("img");
                 memory.img.src = "pics/0.png";
+                memory.img.id = "down";
                 memory.img.className = "pics/"+memory.randArray[memory.count]+".png";
                 memory.count += 1;
                 
-               memory.img.onclick = function(img, className){ //http://stackoverflow.com/questions/20497291/javascript-memory-game-image-swapping/20497536#20497536
+               memory.alink.onclick = function(img, className){ //http://stackoverflow.com/questions/20497291/javascript-memory-game-image-swapping/20497536#20497536
                    return function(){
-                    memory.flipTile(img, className);
+                        memory.flipTile(img, className);
+                        memory.flippedImages += 1;
                    };
                }(memory.img, memory.img.className);
-
-                memory.cell.appendChild(memory.img);
+               
+                memory.alink.appendChild(memory.img);
+                memory.cell.appendChild(memory.alink);
                 row.appendChild(memory.cell);
             }
             
@@ -53,30 +63,46 @@ var memory = {
     },
     
     flipTile:function(img, classname){
-        memory.test +=1;
+
+       
+        if(memory.flippedImages === 2){
+            memory.flippedImages = 0;
+        }
+        console.log(memory.flippedImages);
+        
         img.src = classname;
-        if(memory.test === 1)
+        img.id = "up"+memory.testerino;
+        memory.testerino += 1;
+        if(memory.flippedImages === 0)
+        {memory.test1 = img.src}
+        if(memory.flippedImages === 1)
         {
-            memory.firsttry = classname;
-        }
-        if(memory.test === 2)
-        {
-            memory.secondtry = classname;
-            if(memory.firsttry === memory.secondtry)
-            {}
-            else{
-                img.src = "pics/0.png"
-                memory.firsttry = null;
-                memory.secondtry = null;
+            memory.test2 = img.src
+            if(memory.test1 === memory.test2){
+                console.log("hej")
+                document.getElementById("up0").id="down";
+                document.getElementById("up1").id="down";
+                memory.testerino = 0;                
             }
-        }
+            else{
+                setTimeout(memory.flipBack, 1000);
+            }
+        }  
+        
+        console.log(memory.test1 + " "+ memory.test2);
+        
     },
     
-
-    
-    
-    
-    
+    flipBack:function(){
+        console.log(memory.test1 +" "+ memory.test2);
+        console.log("heeeeeeeeeeeeeeeej");
+        document.getElementById("up0").src="pics/0.png";
+        document.getElementById("up1").src="pics/0.png";
+        document.getElementById("up0").id="down";
+        document.getElementById("up1").id="down";
+        memory.testerino = 0;
+    }
+   
 }
 
 window.onload = memory.init;
