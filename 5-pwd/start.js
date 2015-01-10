a69de28a684122218d46e10e9b6f43519b7e2eff
Jonas Tornfors <jt222ii@student.lnu.se>
2015-01-10 15:39:16 +0000

    added a button to revert to starter background

--- 5-pwd/start.js
+++ 5-pwd/start.js
@@ -2,6 +2,11 @@
 window.onload = function(){
   var desktop = new Desktop();
   
+  var resetbackground = document.querySelector(".resetbackground");
+  resetbackground.onclick = function(){
+    document.body.style.backgroundImage = "url(pictures/bakgrund1.jpg)";
+  };
+  
   desktop.addWindow("bilder", "pictures/photo.svg", Gallery);
   desktop.addWindow("RSS - Barometern", "pictures/rss.svg", Rss);
   desktop.addWindow("RSS - FZ.se", "pictures/rss.svg", Rss2);
